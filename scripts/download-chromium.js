96ed534f4ae69e37f9874e07cf4845ffb1119646
Joseph Manriquez <ManriquezJoeA@gmail.com>
2025-06-21 21:54:30 -0600

    chore: remove legacy chromium download script

--- scripts/download-chromium.js
+++
@@ -1,7 +0,0 @@
-const puppeteer = require('puppeteer-core');
-
-(async () => {
-  console.log('Downloading Chromium revision 884014...');
-  await puppeteer.createBrowserFetcher().download('884014');
-  console.log('Chromium downloaded successfully!');
-})();
