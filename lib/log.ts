08a52f867353566c0b00ade0f5101689d399eb55
Joseph Manriquez <ManriquezJoeA@gmail.com>
2025-06-21 23:56:41 -0600

    Add debug logger and lock @types/react

---
+++ lib/log.ts
@@ -0,0 +1,5 @@
+import debug from 'debug';
+
+export const logParse = debug('jump2:parse'); // for Puppeteer-related logging
+export const logApi = debug('jump2:api');     // for general API request logs
+export const logRender = debug('jump2:render'); // for deployment-specific insights
